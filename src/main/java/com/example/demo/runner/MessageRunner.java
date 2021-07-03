2f72e6c1a7ee02c15fe30b6bfd0ae799ec3ba83c
prajyot baradkar <prajyotbaradkar@gmail.com>
2021-07-03 19:41:17 +0530

    wh 1000 commit two

--- src/main/java/com/example/demo/runner/MessageRunner.java
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -6,8 +6,10 @@ public class MessageRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		int s = 20;
 		System.out.println("welcome to spring boot application");
-
+		
 	}
 
 }
